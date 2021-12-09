03929e4cff18ad453abb68988f1e35b27c260639
David Chinweike <dave@Davids-MacBook-Pro.local>
2021-12-10 00:41:18 +0100

    project-initilization

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useColorMode } from '@chakra-ui/react';
+import { useEffect } from 'react';
+
+import { HomeView } from './views';
 
 function App() {
+
+  const { toggleColorMode } = useColorMode();
+
+  useEffect(() => {
+    toggleColorMode();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HomeView />
   );
 }
 
